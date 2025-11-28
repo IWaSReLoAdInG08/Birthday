7d2609142219fc5d8e55d45bb3bc84a36667c1ab
TheVirgoGuy <102357197+IWaSReLoAdInG08@users.noreply.github.com>
2025-11-29 01:57:39 +0530

    Added my dob

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Jul 08, 2025',
-  name: 'Sidharth (Badebabuuuuu)'
+  birthdate: 'Mar 08, 2026',
+  name: 'Shinu'
 };
 
 function hideEverything() {
